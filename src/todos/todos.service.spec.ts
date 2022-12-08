a1fa2f3e350fbd3865abde1b6cbacaadd92b7bf7
ChoiWonYu <phillip07290@naver.com>
2022-12-08 23:24:33 +0900

    todos

--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -10,9 +10,20 @@ describe('TodosService', () => {
     }).compile();
 
     service = module.get<TodosService>(TodosService);
+    service.create({
+      action: 'study',
+    });
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should get a todo', () => {
+    const todo = service.getOne(1);
+    expect(todo.id).toEqual(1);
+  });
+  it('should return 404 error', () => {
+    try {
+      service.getOne(999);
+    } catch (e) {
+      expect(e.message).toEqual('wrong id : 999');
+    }
   });
 });
